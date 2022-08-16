297377818e95ca03d1324693ef3998abfaedf135
Vitor Natario <natariovitor@gmail.com>
2022-08-16 17:56:19 -0300

    feat: use post in axios

--- script.js
+++ script.js
@@ -9,4 +9,19 @@ const getUsers = () => {
     .catch((e) => console.error(e));
 };
 
+const addNewUser = (newUser) => {
+  axios
+    .post(url, newUser)
+    .then((response) => console.log(response.data))
+    .catch((e) => console.error);
+};
+
+const newUser = {
+  name: "Vitor Natario",
+  avatar:
+    "http://pm1.narvii.com/7029/191980f055b3b696bbe27c94c73cfc075e7fa37er1-736-736v2_uhq.jpg",
+  city: "São Paulo",
+};
+
 getUsers();
+// addNewUser(newUser);
